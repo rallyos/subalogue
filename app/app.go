1fbdd9eb2da44d6eca0bfd445844ee5df16f6668
Dimitar Ralev <dralev@shiftingphotons.dev>
2022-06-30 22:49:14 +0300

    Try not using godotenv outside of tests

--- app/app.go
+++ app/app.go
@@ -3,9 +3,6 @@ package app
 import (
 	"log"
 	"net/http"
-	"os"
-
-	"github.com/joho/godotenv"
 
 	"database/sql"
 	"subalogue/app/middlewares"
@@ -20,20 +17,10 @@ type Server struct {
 }
 
 func (s *Server) Initialize() {
-	setEnv()
 	db.Init()
 	s.Router = mux.NewRouter()
 	s.DB = db.GetConnection()
 	s.routes()
-}
-
-func setEnv() {
-	// Hardcode root folder because there are issues with running the tests (expected behaviour in Go because of the working directory value when tests in other packages are run)
-	// We use containers so we have a guarantee that this path will always be correct
-	err := godotenv.Load("/app/.env." + os.Getenv("SUBALOGUE_ENV"))
-	if err != nil {
-		log.Fatal(err.Error())
-	}
 }
 
 func (s *Server) Run() {
