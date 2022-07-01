26e4d3c59fab4c5168f60d6776a506efbbd3ca0a
Dimitar Ralev <dralev@shiftingphotons.dev>
2022-07-01 13:37:33 +0300

    Fix build script

--- render.sh
+++ render.sh
@@ -1,3 +1,3 @@
 go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
-migrate -source file:///db/migrations -database ${DATABASE_URL} up
+migrate -source file://db/migrations -database ${DATABASE_URL} up
 go build .
